a97163f04a0870f621d4bc7d348ca725ca40b8f8
yonmoyonmo <yonmosound@gmail.com>
2021-08-29 20:26:37 +0900

    endpoint changed

--- lib/utils/SmallTalkHelperEndpoint.dart
+++ lib/utils/SmallTalkHelperEndpoint.dart
@@ -1,9 +1,9 @@
 class SmallTalkHelperEndpoint {
-  // final String host =
-  //     "https://small-talk-helper.wonmonae.com/api/sugguestion/small-talk-helper/";
-
   final String host =
-      "http://172.30.1.25:5000/api/sugguestion/small-talk-helper/";
+      "https://small-talk-helper.wonmonae.com/api/sugguestion/small-talk-helper/";
+
+  // final String host =
+  //     "http://172.30.1.25:5000/api/sugguestion/small-talk-helper/";
 
   Uri getEndpoint(String endpointName) {
     String uri = this.host + endpointName;
